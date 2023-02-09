36c89f8cd050945f4d381b93ad89b9afe16706ff
Oleg <rbghero@gmail.com>
2023-02-09 17:01:14 +0300

    fix bug

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,15 +4,16 @@ public class SQRService {
     int bottomLine = 10;
     int topLine = 99;
 
-    public int calcSqr(int x, int y) {
+    public int calcSqr(int StartLimit, int LastLimit) {
         int range = 0;
-        int b = 0;
+        int ValuableCount = 0;
         for (int i = bottomLine; i <= topLine; i++) {
-            b = i * i;
-            if (b >= x && b <= y) {
-                range = range + 1;;
+            ValuableCount = i * i;
+            if (ValuableCount >= StartLimit && ValuableCount <= LastLimit) {
+                range = range + 1;
             }
         }
         return range;
+
     }
 }
